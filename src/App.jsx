2da9d225dabfce5875db1a6280326b1a54b1955b
Bence Pásztor <bence7661@gmail.com>
2025-07-17 00:32:22 +0200

    Cleans up base `.css` stuff.

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,21 @@ import NavBar from './components/NavBar/NavBar.jsx'
 function App() {
 
   return (
-      <BrowserRouter>
-        <div>
-          <NavBar/>
+    <BrowserRouter>
+      <div className='initial-viewport'>
+        <NavBar />
+        <div className='body-wrapper'>
+          <main>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="Home" element={<Home />} />
+              <Route path="Notes" element={<Notes />} />
+              <Route path="Register" element={<Register />} />
+            </Routes>
+          </main>
         </div>
-        <main>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="Home" element={<Home />} />
-            <Route path="Notes" element={<Notes />} />
-            <Route path="Register" element={<Register />} />
-          </Routes>
-        </main>
-      </BrowserRouter>
+      </div>
+    </BrowserRouter>
   )
 }
 
