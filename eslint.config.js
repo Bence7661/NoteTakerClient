45c4f51647d846928418818c78c78e9b98ee646d
Bence Pásztor <bence7661@gmail.com>
2025-07-16 23:13:29 +0200

    Installs and configures eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react';
+
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -8,10 +10,13 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      react
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      reactRefresh.configs.vite
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -24,6 +29,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-undef': 'error',
+      'react/jsx-no-undef': 'error'
     },
   },
 ])
