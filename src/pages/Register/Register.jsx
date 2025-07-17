48030ef545ecd5daf9ab3c25bdd0f4f5cdfb65b9
Bence Pásztor <bence7661@gmail.com>
2025-07-17 22:23:07 +0200

    Creates `Register` page's layout.

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,22 @@
+import InputField from "../../components/InputField/InputField";
+import Card from "../../components/Card/Card";
 import "./Register.css";
 
 function Register() {
   return (
-    <>
-    </>
+    <div className="register-page">
+      <Card title="Registration">
+        <InputField
+          label="Username"
+          type="text"
+          placeholder="Enter your username"
+        />
+        <InputField
+          label="Password"
+          type="password"
+        />
+      </Card>
+    </div>
   );
 }
 
