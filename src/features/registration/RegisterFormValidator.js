d6fc08d1961f752756181f1bf6f84359e9004c12
Bence Pásztor <bence7661@gmail.com>
2025-07-27 21:30:33 +0200

    Adds validator to separate validation logic.

---
+++ src/features/registration/RegisterFormValidator.js
@@ -0,0 +1,68 @@
+export function Validate(formData, onlyThisField = null) {
+    if (formData == null) {
+        console.log("No data passed to validation!");
+        return;
+    }
+    const errors = [];
+
+    switch (onlyThisField) {
+        case "username":
+            ValidateUsernameField(
+                formData.username,
+                errors
+            );
+            break;
+        case "password":
+            ValidatePasswordField(
+                formData.password,
+                errors
+            );
+            break;
+        case "confirmPassword":
+            ValidateConfirmPasswordField(
+                formData.confirmPassword,
+                formData.password,
+                errors
+            );
+            break;
+        default:
+            ValidateUsernameField(
+                formData.username,
+                errors
+            );
+            ValidatePasswordField(
+                formData.password,
+                errors
+            );
+            ValidateConfirmPasswordField(
+                formData.confirmPassword,
+                formData.password,
+                errors
+            );
+            break;
+    }
+
+    return errors;
+}
+
+function ValidateUsernameField(username, errors) {
+    if (!username.trim()) errors.username = "Required";
+}
+
+function ValidatePasswordField(password, errors) {
+    if (!password) {
+        errors.password = "Required";
+    }
+    else if (password.length < 6) {
+        errors.password = "Min 6 characters";
+    }
+}
+
+function ValidateConfirmPasswordField(confirmPassword, password, errors) {
+    if (!confirmPassword) {
+        errors.confirmPassword = "Required";
+    }
+    else if (confirmPassword != password) {
+        errors.confirmPassword = "Must match password";
+    }
+}
